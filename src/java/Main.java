bd4b4387dc910708b37f2a790bf8866d8925a06e
Anton Samsonov <asamsonov03@yandex.ru>
2019-06-03 20:57:16 +0300

    create Main and CountYZ

---
+++ src/java/Main.java
@@ -0,0 +1,10 @@
+import Strings.CountYZ;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(new CountYZ().start("day fyyyz"));
+
+    }
+}
---
+++ src/java/Strings/CountYZ.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class CountYZ {
+
+    public int start(String str) {
+        str = str.toLowerCase();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'y' || str.charAt(i) == 'z') {
+                if (i == str.length() - 1 || !Character.isLetter(str.charAt(i + 1))) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
