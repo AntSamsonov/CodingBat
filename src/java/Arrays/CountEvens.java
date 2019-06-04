516b7ed890d21bbdf41dce5d4f9921955b59f62f
Anton Samsonov <asamsonov03@yandex.ru>
2019-06-04 09:16:48 +0300

    create sout in Main and Solution CountHi, BobThere, CountEvens

---
+++ src/java/Arrays/CountEvens.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class CountEvens {
+    public int start(int[] nums){
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] % 2 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
--- src/java/Main.java
+++ src/java/Main.java
@@ -1,12 +1,21 @@
+import Arrays.CountEvens;
+import Strings.BobThere;
+import Strings.CountHi;
 import Strings.CountYZ;
 import Strings.DoubleChar;
 
 public class Main {
 
     public static void main(String[] args) {
-
+                //String-2
         System.out.println(new CountYZ().start("day fyyyz"));
         System.out.println(new DoubleChar().start("abc"));
+        System.out.println(new BobThere().start("bib"));
+        System.out.println(new CountHi().start("hilvrepdfhisdvlkszdhi"));
+
+                //Array-2
+        System.out.println(new CountEvens().start(new int[]{1, 2, 4, 5}));
+
 
     }
 }
